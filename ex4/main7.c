218a3b50f9f1588dd5982a45dd35eb1d1158504b
B1229056 <144075644+GinoHuangs@users.noreply.github.com>
2025-04-10 15:33:18 +0800

    Add files via upload

---
+++ ex4/main7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int multi(int i, int j) {
+    if (j == 1) {
+        printf("%d*%d=%d\n", i, j, i*j);
+        return 0 ;
+    } else {
+        multi(i, j-1);
+        printf("%d*%d=%d\n", i, j, i*j);    //preorder or postorder?
+    }
+}
+
+int main() {
+    multi(1, 9);
+    return 0;
+}
